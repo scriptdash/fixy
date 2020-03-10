2860b765c3fa364749a6303fd0993e484d0ddecc
Mischa Serlin <mischa.serlin@alto.com>
2020-03-10 07:19:41 -0700

    add numeric format

--- lib/fixy.rb
+++ lib/fixy.rb
@@ -1,6 +1,7 @@
 require 'fixy/decorator/debug'
 require 'fixy/decorator/default'
 require 'fixy/document'
-require 'fixy/formatter/formats'
+require 'fixy/formatter/alphanumeric'
+require 'fixy/formatter/numeric'
 require 'fixy/record'
 require 'fixy/version'
